166207cc29a64ac79bda86dcdff9abfd03999b42
Etienne Robin <etienner0601@gmail.com>
2021-05-04 00:46:21 -0700

    Uses getopt library now

--- remove-chars.cpp
+++ remove-chars.cpp
@@ -1,39 +1,71 @@
 #include <iostream>
+#include <ostream>
 #include <fstream>
 #include <cstdlib>
+#include <string>
+#include <getopt.h>
 using namespace std;
 
-void print_usage() {
-	cerr << "Usage: remove-chars [chars] [inFile] [outFile]" << endl;
+void print_usage(char* name) {
+	fprintf(stderr, "Usage: %s [-o outFile] charString inFile", name);
 }
 
 int main(int argc, char** argv) {
 
-    if (argc != 4) {
-        print_usage();
-        exit(EXIT_FAILURE);
-    }
+    int opt;
+    int o_flag = 0;
+    char* outFileName;
 
-    string removeChars(argv[1]);
-    int nextByte;
-
-    ifstream toRead;
-    toRead.open(argv[2], ios::binary); // open the input file to read from
-
-    ofstream toWrite;
-    toWrite.open(argv[3], ios::binary); // open the output file to write to
-
-
-    while((nextByte = toRead.get()) != EOF) {
-        if (removeChars.find((char)nextByte) == string::npos) {
-            toWrite.put((char)nextByte);
+    while ((opt = getopt(argc, argv, "o:")) != -1) {
+        switch (opt) {
+            case 'o':
+                o_flag = 1;
+                outFileName = optarg;
+                break;
+            case '?':
+                print_usage(argv[0]);
+                exit(EXIT_FAILURE);
         }
     }
 
-    toWrite.flush();
+    if (o_flag) {
+        if (argc != 5) {
+            print_usage(argv[0]);
+            exit(EXIT_FAILURE);
+        }
+    } else {
+        if (argc != 3) {
+            print_usage(argv[0]);
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    string removeChars(argv[optind]);
+    int nextByte;
+
+    ifstream toRead;
+    toRead.open(argv[optind + 1], ios::binary); // open the input file to read from
+
+    if (o_flag) {
+        string outFile(outFileName);
+        ofstream toWrite;
+        toWrite.open(outFile, ios::binary); // open the output file to write to
+        while ((nextByte = toRead.get()) != EOF) {
+            if (removeChars.find((char)nextByte) == string::npos) {
+                toWrite.put((char)nextByte);
+            }
+        }
+        toWrite.flush();
+        toWrite.close();
+    } else {
+        while ((nextByte = toRead.get()) != EOF) {
+            if (removeChars.find((char)nextByte) == string::npos) {
+                cout.put((char)nextByte);
+            }
+        }
+    }
 
     toRead.close();
-    toWrite.close();
 
     return 0;
 }
